e849429eb1f9e4846bae2f4e8c5ffe488703638f
Levi <levi.ciarrocchi@gmail.com>
2021-04-27 13:54:25 -0300

    Pastas

---
+++ Cap 8 - Fase 3/Condicional/index.js
@@ -0,0 +1,14 @@
+/**
+ * Você foi contratado para desenvolver um trabalho: um serviço em que as pessoas poderão usar um estúdio profissional
+ * para gravar seus vídeos para o YouTube com máxima qualidade. O serviço ganha dinheiro por meio de um sistema de
+ * assinaturas e de um bônus calculado por uma porcentagem sobre o faturamento que o canal do cliente teve ao longo do ano.
+
+   Sua tarefa é criar um algoritmo que receba o tipo de assinatura do cliente, além de seu faturamento anual, e calcule
+   e exiba qual o valor do bônus que o cliente deve pagar a vocês. A tabela abaixo mostra a porcentagem aplicada de
+   acordo com cada nível de assinatura:
+
+   Basic -> 30%
+   Silver -> 20%
+   Gold -> 10%
+   Platinum -> 5%
+ */

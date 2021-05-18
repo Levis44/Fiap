2d2bd190db94353e8b637cfd4ff227a8b6fd3282
Levi <levi.ciarrocchi@gmail.com>
2021-05-17 21:34:20 -0300

    Classes

--- Nave/index.js
+++ Nave/index.js
@@ -1,11 +1,62 @@
 const canvas = document.getElementById("tela");
 const ctx = canvas.getContext("2d");
 
-// let x = 0;
-// let y = 150;
+class ClassNave {
+  constructor() {
+    this.x = 350;
+    this.y = 150;
+    this.image = "nave-cima.png";
+    this.placar = 0;
+    this.hit = false;
+  }
 
-// let placar = 0;
-// let hit = false;
+  desenharNave(pX, pY) {
+    ctx.clearRect(0, 0, 800, 400); //antes de fazer o desenho é preciso limpar o canvas
+    const imagem = new Image();
+    imagem.src = this.image;
+    ctx.drawImage(imagem, pX, pY);
+  }
+}
+const Nave = new ClassNave();
+
+class Keyboard {
+  static ArrowUp() {
+    if (Nave.y > 0) {
+      Nave.image = "nave-cima.png";
+      Nave.y -= 20;
+    }
+  }
+
+  static ArrowDown() {
+    if (Nave.y < 300) {
+      Nave.image = "nave-baixo.png";
+      Nave.y += 20;
+    }
+  }
+
+  static ArrowRight() {
+    if (Nave.x < 691) {
+      Nave.image = "nave-direita.png";
+      Nave.x += 20;
+    }
+  }
+
+  static ArrowLeft() {
+    if (Nave.x > 0) {
+      Nave.image = "nave-esquerda.png";
+      Nave.x -= 20;
+    }
+  }
+
+  static handleKeyDown(event) {
+    const keyPressed = event.key;
+    const move = Keyboard[keyPressed];
+
+    if (move) {
+      move();
+    }
+  }
+}
 
 // requestAnimationFrame(gameloop);
 // function gameloop() {
@@ -16,7 +67,7 @@ const ctx = canvas.getContext("2d");
 //   }
 
 //   x += 10;
-//   requestAnimationFrame(gameloop);
+//   requestAnimationvar fps = 1;Frame(gameloop);
 // }
 
 // function desenharQuadrado(pX, pY) {
@@ -29,66 +80,38 @@ const ctx = canvas.getContext("2d");
 // }
 
 // ---------------------------------------------------------------------------
-let x = 0;
-let y = 150;
-let image = "nave-cima.png";
 //a função gameloop é chamada aqui
 requestAnimationFrame(gameloop);
-
-window.onkeydown = pressionaTecla;
-function pressionaTecla(tecla) {
-  if (tecla.keyCode == 38) {
-    image = "nave-cima.png";
-    y = y - 5; //diminuir y tem o efeito de subida
-  }
-  if (tecla.keyCode == 40) {
-    image = "nave-baixo.png";
-    y = y + 5; //aumentar y tem o efeito de descer
-  }
-  if (tecla.keyCode == 39) {
-    image = "nave-direita.png";
-    x = x + 5; //aumentar o x tem o efeito de ir para a direita
-  }
-  if (tecla.keyCode == 37) {
-    image = "nave-esquerda.png";
-    x = x - 5; //diminuir o x tem o efeito de ir para a esquerda
-  }
-}
-
 function gameloop() {
-  desenharNave(x, y);
+  window.onkeydown = Keyboard.handleKeyDown;
+
+  const x = Nave.x;
+  const y = Nave.y;
+  Nave.desenharNave(x, y);
 
   //chama novamente o ciclo da animação
   requestAnimationFrame(gameloop);
 }
 
-function desenharNave(pX, pY) {
-  ctx.clearRect(0, 0, 800, 400); //antes de fazer o desenho é preciso limpar o canvas
-  ctx.fillStyle = "#00F";
-
-  const imagem = new Image();
-  imagem.src = image;
-  ctx.drawImage(imagem, pX, pY);
-}
-
 // ----------------------------------------------------------------------------
 // ASTEROIDES ALEATORIOS
 
-// var fps = 1;
-// // var fps = 1 / 3;
-// var xQ, yQ;
-// function desenharQ() {
-//   setTimeout(function () {
-//     requestAnimationFrame(desenharQ);
-//     // Drawing code goes here
-//     console.log(xQ + " - " + yQ);
-//     ctx.clearRect(xQ, yQ, 30, 30);
-//     xQ = Math.floor(Math.random() * canvas.width);
-//     yQ = Math.floor(Math.random() * canvas.height);
-//     ctx.fillStyle = "red";
-//     ctx.fillRect(xQ, yQ, 30, 30);
-//   }, 1000 / fps);
-// }
+var fps = 1;
+// var fps = 1 / 3;
+var xQ, yQ;
+function desenharQ() {
+  setTimeout(function () {
+    requestAnimationFrame(desenharQ);
+    // Drawing code goes here
+    console.log(xQ + " - " + yQ);
+    ctx.clearRect(xQ, yQ, 30, 30);
+    xQ = Math.floor(Math.random() * canvas.width);
+    yQ = Math.floor(Math.random() * canvas.height);
+    ctx.fillStyle = "red";
+    ctx.fillRect(xQ, yQ, 30, 30);
+  }, 1000 / fps);
+}
+desenharQ();
 
 // ASTEROIDES ALEATORIOS
 // ----------------------------------------------------------------------------
